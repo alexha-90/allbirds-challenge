1a242d72d31d36a824f499be017f58457ba02bec
Alex Ha <alex.ha@volvocars.com>
2021-08-13 21:54:35 -0700

    working on first event handler for form input

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 //====================================================================================================================//
 
+const INPUT_FIELDS = {
+    FIRST_NAME: "firstName",
+    LAST_NAME: "lastName",
+    PHONE_NUMBER: "phoneNumber",
+    EMAIL_ADDRESS: "emailAddress",
+    PASSWORD: "password",
+    PASSWORD_CONFIRMATION: "passwordConfirmation"
+};
+
 function LoginPage() {
+
+    const [formValues, setFormValues] = useState({
+        [INPUT_FIELDS.FIRST_NAME]: "",
+        [INPUT_FIELDS.LAST_NAME]: "",
+        [INPUT_FIELDS.PHONE_NUMBER]: "",
+        [INPUT_FIELDS.EMAIL_ADDRESS]: "",
+        [INPUT_FIELDS.PASSWORD]: "",
+        [INPUT_FIELDS.PASSWORD_CONFIRMATION]: ""
+    });
+
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.id);
+        console.log(e.target.value);
+        // setFormValues({
+        //     [INPUT_FIELDS.FIRST_NAME]: ""
+        // });
+        // setFormValues({
+        //     ...formValues,
+        //     [e.target.id]: [e.target.value]
+        // })
+
+
+    };
+
+
+
     return (
         <div className="login-page">
             <h1>CREATE AN ACCOUNT</h1>
@@ -11,8 +46,20 @@ function LoginPage() {
             </p>
             <div className="form-wrapper">
                 <form>
+                    <label>
+                        FIRST NAME
+                        <input
+                            type="text"
+                            maxLength={50}
+                            id={INPUT_FIELDS.FIRST_NAME}
+                            autoComplete="given-name"
+                            value={formValues[INPUT_FIELDS.FIRST_NAME]}
+                            onChange={handleOnChange}
+                        />
+                    </label>
 
 
+                    {/*<input type="submit" value="REGISTER" />*/}
                 </form>
             </div>
         </div>
@@ -20,3 +67,16 @@ function LoginPage() {
 }
 
 export default LoginPage;
+
+
+/*
+                        <input
+                            name="email"
+                            type="email"
+                            id="}
+                            maxLength={50}
+                            autoComplete="given-name"
+                            value={""}
+                            onChange={handleOnChange}
+                        />
+ */
