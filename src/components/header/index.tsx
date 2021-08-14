85cc2ac7d92663e60c1c3c83faf66382a2eea322
Alex Ha <alex.ha@volvocars.com>
2021-08-13 21:09:36 -0700

    more styling for header and footer

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,7 +6,28 @@ import './style.scss';
 function Header() {
     return (
         <div className="header">
-            <img src={AllbirdsLogo} alt="logo" />
+            <div className="left-content">
+                <button>
+                    <span>MEN</span>
+                </button>
+                <button>
+                    <span>WOMEN</span>
+                </button>
+                <button>
+                    <span>NEW ARRIVALS</span>
+                </button>
+            </div>
+            <div className="middle-content">
+                <img src={AllbirdsLogo} alt="logo" />
+            </div>
+            <div className="right-content">
+                <button>
+                    <span>SUSTAINABILITY</span>
+                </button>
+                <button>
+                    <span>STORES</span>
+                </button>
+            </div>
         </div>
     );
 }
